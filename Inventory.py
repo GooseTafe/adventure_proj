3b4e8245171a35371b7173dbc94385f7b19c753e
Alexander <20085553@tafe.wa.edu.au>
2022-10-18 12:11:16 +0800

    added search to inventory

--- Inventory.py
+++ Inventory.py
@@ -1,5 +1,22 @@
 class Inventory:
     def __init__(self):
-        self.items =[]
+        self.items = ["test1", "test2", "test3"]
 
-    # TODO: add search to search inventory
+    # Binary search through inventory
+    def search_inventory(self, item):
+        sorted_items = sorted(self.items)
+        start = 0
+        end = len(sorted_items) - 1
+
+        while start <= end:
+            middle = int((start + end) / 2)
+            midpoint = sorted_items[middle]
+            if midpoint > item:
+                end = middle - 1
+            elif midpoint < item:
+                start = middle + 1
+            else:
+                return midpoint
+
+
+
